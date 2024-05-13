475cda74dac5dc5fced4db8db6964f566cd694e0
James Cook <cookjws@gmail.com>
2024-05-13 17:56:25 +0100

    tiling by column blocks works but is it faster

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using MPIQR
 
-using LinearAlgebra, Random, Base.Threads
+using LinearAlgebra, Random, Base.Threads, Test
 using MPI, Distributed, MPIClusterManagers
 
 MPI.Init(;threadlevel=MPI.THREAD_SERIALIZED)
@@ -10,48 +10,77 @@ const sze = MPI.Comm_size(cmm)
 const nts = Threads.nthreads()
 
 BLAS.set_num_threads(nts)
+
+using Random
+
 @static if Sys.islinux()
   using ThreadPinning
-  cpus = rnk * nts:(rnk + 1) * nts
-  ThreadPinning.pinthreads(cpus)
 end
 
-#include("qrmpi.jl")
-using Random
-Random.seed!(0)
+function run()
+  for blocksize in (1, 2, 3, 4), npow in 8:2:12, T in (ComplexF64, )#Float64,
+    @static if Sys.islinux()
+      cpus = rnk * nts:(rnk + 1) * nts
+      ThreadPinning.pinthreads(cpus)
+    end
 
-for blocksize in (1, 4), npow in 7:1:10, T in (ComplexF64, )#Float64, 
-  n = 2^npow
-  m = n + 2^(npow-2)
-  iszero(rnk) && @show T, m, n, blocksize
-  A0 = zeros(T, 0, 0)
-  x1 = b0 = zeros(T, 0)
-  if rnk == 0
-    A0 = rand(T, m, n)
-    b0 = rand(T, m)
-    A1 = deepcopy(A0)
-    b1 = deepcopy(b0)
-    t1 = @elapsed x1 = qr!(A1, NoPivot()) \ b1
-  end
-  Aall = MPI.bcast(A0, 0, cmm)
-  ball = MPI.bcast(b0, 0, cmm)
-  xall = MPI.bcast(x1, 0, cmm)
-  x1 = xall
+    Random.seed!(0)
+    n = (2^npow ÷ blocksize) * blocksize
+    m = n + 2^(npow-2)
+    A0 = zeros(T, 0, 0)
+    x1 = b0 = zeros(T, 0)
+    if rnk == 0
+      BLAS.set_num_threads(nts * sze)
+      A0 = rand(T, m, n)
+      b0 = rand(T, m)
+      A1 = deepcopy(A0)
+      b1 = deepcopy(b0)
+      x1 = qr!(A1, NoPivot()) \ b1
+      t1s = []
+      for _ in 1:3
+        push!(t1s, @elapsed qr!(A1, NoPivot()) \ b1)
+      end
+      t1 = minimum(t1s)
+      BLAS.set_num_threads(nts)
+    end
+    Aall = MPI.bcast(A0, 0, cmm)
+    ball = MPI.bcast(b0, 0, cmm)
+    xall = MPI.bcast(x1, 0, cmm)
+    x1 = xall
 
-  localcols = MPIQR.localcolumns(rnk, n, blocksize, sze)
-  b = deepcopy(ball)
+    localcols = MPIQR.localcolumns(rnk, n, blocksize, sze)
+    b = deepcopy(ball)
 
-  A = MPIQR.MPIQRMatrix(deepcopy(Aall[:, localcols]), size(Aall); blocksize=blocksize)
+    A = MPIQR.MPIQRMatrix(deepcopy(Aall[:, localcols]), size(Aall); blocksize=blocksize)
 
-  t2 = @elapsed  begin
+    MPI.Barrier(cmm)
     H, α = MPIQR.householder!(A)
     x2 = MPIQR.solve_householder!(b, H, α)
-  end
-  if iszero(rnk)
-    @show norm(Aall' * Aall * x1 .- Aall' * ball)
-    @show norm(Aall' * Aall * x2 .- Aall' * ball)
-    @show t2 / t1
+
+    t2s = []
+    for _ in 1:3
+      push!(t2s, @elapsed begin
+        H, α = MPIQR.householder!(A)
+        MPIQR.solve_householder!(b, H, α)
+      end)
+    end
+    t2 = minimum(t2s)
+
+    MPI.Barrier(cmm)
+
+    if iszero(rnk)
+        @assert norm(Aall' * Aall * x1 .- Aall' * ball) < 1e-8
+        res = norm(Aall' * Aall * x2 .- Aall' * ball)
+        try
+          println("np=$sze, nt=$nts, T=$T, m=$m, n=$n, blocksize=$blocksize took $(t2/t1)x")
+          @assert res < 1e-8
+          println("    PASSED: norm of residual = $res")
+        catch
+          println("    FAILED: norm of residual = $res")
+        end
+    end
   end
 end
+run()
 
 MPI.Finalize()
