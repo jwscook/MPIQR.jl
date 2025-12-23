09d7caa05926b15bdd06163265537c06e1b5d928
James Cook <cookjws@gmail.com>
2025-12-23 14:33:06 +0000

    re-use LAPACK qr solve in test across blocksizes

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,8 +12,8 @@ const nts = Threads.nthreads()
 BLAS.set_num_threads(nts)
 
 using Random, ProgressMeter
-function run(blocksizes=(1,2,3,4), npows=(8,10), Ts=(ComplexF64,); bestof=4)
-  for npow in npows, blocksize in blocksizes, T in Ts
+function run(blocksizes=(1,2,3,4), npows=(8,10,), Ts=(ComplexF64,); bestof=4)
+  for npow in npows, T in Ts
     Random.seed!(0)
     n = 2^npow
     m = n + 2^(npow-2)
@@ -35,53 +35,57 @@ function run(blocksizes=(1,2,3,4), npows=(8,10), Ts=(ComplexF64,); bestof=4)
     end
     x1 = MPI.bcast(x1, 0, cmm)
 
-    localcols = MPIQR.localcolumns(rnk, n, blocksize, sze)
-    b = deepcopy(b0)
+    for blocksize in blocksizes
+      localcols = MPIQR.localcolumns(rnk, n, blocksize, sze)
+      b = deepcopy(b0)
 
-    A = MPIQR.MPIQRMatrix(deepcopy(A0[:, localcols]), size(A0); blocksize=blocksize)
-    y2 = A * x1
-    if iszero(rnk)
-      @test y2 ≈ y1
-    end
-    MPI.Barrier(cmm)
-    #x2 = qr!(A) \ b
-    dt = iszero(rnk) ? 1 : 2^31
-    x2 = similar(x1)
-    qrA = qr!(A, progress=Progress(A, dt=dt; showspeed=true))
-    ldiv!(x2, qrA, b; verbose=false, progress=Progress(A, dt=dt/10; showspeed=true))
-    x3 = qrA \ b
-    localcols = MPIQR.localcolumns(qrA)
-    qrA.A.localmatrix .= A0[:, localcols] # re-use it
-    @test all(qrA[:, localcols] .== A0[:, localcols]) # re-use it
-    fill!(qrA.A.localmatrix, -1) # fill with distintive value and test we overwrite it
-    qrA[:, localcols] .= A0[:, localcols] # re-use it
-    @test x2 ≈ x3
-    @test all(qrA[:, localcols] .== A0[:, localcols]) # re-use it
-    qrA[1, localcols[1]] = π
-    @test qrA[1, localcols[1]] == T(π)
+      A = MPIQR.MPIQRMatrix(deepcopy(A0[:, localcols]), size(A0); blocksize=blocksize)
+      y2 = A * x1
+      if iszero(rnk)
+        @test y2 ≈ y1
+      end
+      MPI.Barrier(cmm)
+      #x2 = qr!(A) \ b
+      dt = iszero(rnk) ? 1 : 2^31
+      x2 = similar(x1)
+      qrA = qr!(A; verbose=false, progress=Progress(A, dt=dt; showspeed=true))
+      ldiv!(x2, qrA, b; verbose=false, progress=Progress(A, dt=dt/10; showspeed=true))
+      x3 = qrA \ b
+      localcols = MPIQR.localcolumns(qrA)
+      if !isempty(localcols)
+      qrA.A.localmatrix .= A0[:, localcols] # re-use it
+      @test all(qrA[:, localcols] .== A0[:, localcols]) # re-use it
+      fill!(qrA.A.localmatrix, -1) # fill with distintive value and test we overwrite it
+      qrA[:, localcols] .= A0[:, localcols] # re-use it
+      @test x2 ≈ x3
+      @test all(qrA[:, localcols] .== A0[:, localcols]) # re-use it
+      qrA[1, localcols[1]] = π
+      @test qrA[1, localcols[1]] == T(π)
+      end
 
-    t2s = []
-    for _ in 1:bestof
-      push!(t2s, @elapsed begin
-        progress = Progress(A, dt=dt; showspeed=true)
-        qr!(A; progress=progress) \ b
-      end)
-    end
-    t2 = minimum(t2s)
+      t2s = []
+      for _ in 1:bestof
+        push!(t2s, @elapsed begin
+          progress = Progress(A, dt=dt; showspeed=true)
+          qr!(A; progress=progress) \ b
+        end)
+      end
+      t2 = minimum(t2s)
 
-    MPI.Barrier(cmm)
+      MPI.Barrier(cmm)
 
-    if iszero(rnk)
-        @test norm(A0' * A0 * x1 .- A0' * b0) < 1e-8
-        res = norm(A0' * A0 * x2 .- A0' * b0)
-        try
-          println("np=$sze, nt=$nts, T=$T, m=$m, n=$n, blocksize=$blocksize:")
-          println("tLAPACK = $t1, tMPIQR = $t2, ratio=$(t2/t1)x")
-          @test res < 1e-8
-          println("    PASSED: norm of residual = $res")
-        catch
-          println("    FAILED: norm of residual = $res")
-        end
+      if iszero(rnk)
+          @test norm(A0' * A0 * x1 .- A0' * b0) < 1e-8
+          res = norm(A0' * A0 * x2 .- A0' * b0)
+          try
+            println("np=$sze, nt=$nts, T=$T, m=$m, n=$n, blocksize=$blocksize:")
+            println("tLAPACK = $t1, tMPIQR = $t2, ratio=$(t2/t1)x")
+            @test res < 1e-8
+            println("    PASSED: norm of residual = $res")
+          catch
+            println("    FAILED: norm of residual = $res")
+          end
+      end
     end
   end
 end
