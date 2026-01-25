6a0adc605fc5433d8bcf905349b6af089459b84a
James Cook <cookjws@gmail.com>
2026-01-25 18:32:03 +0000

    move to calls to gemm and geru where possible

--- ext/MPIQRCUDAExt.jl
+++ ext/MPIQRCUDAExt.jl
@@ -204,76 +204,36 @@ function LinearAlgebra.ldiv!(
 
   return A
 end
-
-function LinearAlgebra.mul!( # fast
+function LinearAlgebra.mul!(
     y::CuArray{T, 2, CUDA.DeviceMemory},
     aHj::Adjoint{T, CuArray{T, 1, CUDA.DeviceMemory}},
     H::SubArray{T, 2, CuArray{T, 2, CUDA.DeviceMemory}, Tuple{UnitRange{Int64}, UnitRange{Int64}}, false}
-    ) where T<:Union{Float32, Float64, ComplexF32, ComplexF64}
-
+    ) where T<:Union{ComplexF32, ComplexF64}
 
   Hj = parent(aHj)
-  m = length(Hj)
-  n = size(H, 2)
 
   @assert size(y, 1) == 1
-  @assert size(y, 2) == n
-  @assert size(H, 1) == m
+  @assert size(y, 2) == size(H, 2)
+  @assert size(H, 1) == length(Hj)
 
-  # Get SubArray parent and offsets
-  H_parent = parent(H)
-  H_row_offset = first(H.indices[1]) - 1
-  H_col_offset = first(H.indices[2]) - 1
+  # Reshape y to a vector (view, no copy)
+  y_vec = view(y, 1, :)
 
-  threads = 256
-  blocks = cld(n, threads)
-
-  @cuda threads=threads blocks=blocks _rowvec_matmul_kernel!(
-    y, Hj, H_parent, m, n, H_row_offset, H_col_offset
-  )
+  # For complex types: y = Hj' * H
+  # We need: y_vec[j] = sum_i conj(Hj[i]) * H[i,j]
+  # This is: y_vec = H' * conj(Hj) using transpose (not conjugate transpose)
+  # OR equivalently: y_vec = conj(H)' * Hj using conjugate transpose
+  # The simplest is: gemv with 'C' (conjugate transpose) on H
+  CUDA.CUBLAS.gemv!('C', one(T), H, Hj, zero(T), y_vec)
 
   return y
-end
-
-function _rowvec_matmul_kernel!(y, Hj, H_parent, m, n, row_off, col_off)
-  T = eltype(y)
-  j = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-
-  if j <= n
-    acc = zero(T)
-
-    # Unrolled loop for better performance
-    i = 1
-    @inbounds while i <= m - 3
-      h1 = conj(Hj[i])
-      h2 = conj(Hj[i+1])
-      h3 = conj(Hj[i+2])
-      h4 = conj(Hj[i+3])
-
-      acc += h1 * H_parent[row_off + i, col_off + j]
-      acc += h2 * H_parent[row_off + i + 1, col_off + j]
-      acc += h3 * H_parent[row_off + i + 2, col_off + j]
-      acc += h4 * H_parent[row_off + i + 3, col_off + j]
-
-      i += 4
-    end
-
-    @inbounds while i <= m
-      acc += conj(Hj[i]) * H_parent[row_off + i, col_off + j]
-      i += 1
-    end
-
-    y[1, j] = acc
-  end
-
-  return
 end
 
 function LinearAlgebra.mul!(
     H::SubArray{T, 2, CuArray{T, 2, CUDA.DeviceMemory}, Tuple{UnitRange{Int64}, UnitRange{Int64}}, false},
     Hj::CuArray{T, 1, CUDA.DeviceMemory},
     z::CuArray{T, 2, CUDA.DeviceMemory},
-    α::Int, β::Bool) where T<:Union{Float32, Float64, ComplexF32, ComplexF64}
+    α::Int, β::Bool) where T<:Union{ComplexF32, ComplexF64}
 
   @assert α == -1 && β == 1 "This kernel is optimized for α=-1, β=1 only"
   @assert size(z, 1) == 1 "z must have first dimension = 1"
@@ -284,11 +244,7 @@ function LinearAlgebra.mul!(
   # Use CUBLAS ger!/geru! for rank-1 update: H = H + α * Hj * z_vec'
   # For real types: use ger!
   # For complex types: use geru! (unconjugated) or gerc! (conjugated)
-  if T <: Union{Float32, Float64}
-    CUDA.CUBLAS.ger!(T(-1), Hj, z_vec, H)
-  else  # Complex types
-    CUDA.CUBLAS.geru!(T(-1), Hj, z_vec, H)
-  end
+  CUDA.CUBLAS.geru!(T(-1), Hj, z_vec, H)
 
   return H
 end
@@ -336,113 +292,23 @@ function Base.copyto!(
   return a
 end
 
-#function Base.copyto!(
-#    a::SubArray{T, 2, CuArray{T, 2, CUDA.DeviceMemory}, Tuple{UnitRange{Int64}, UnitRange{Int64}}, false},
-#    b::SubArray{T, 2, CuArray{T, 2, CUDA.DeviceMemory}, Tuple{UnitRange{Int64}, UnitRange{Int64}}, false}
-#    ) where {T<:Union{Float32, Float64, ComplexF32, ComplexF64}}
-#
-#  @assert size(a, 2) == 1
-#  @assert size(b, 2) == 1
-#  @assert size(a, 1) == size(b, 1)
-#  @assert a.indices[1] == b.indices[1] # a limitation of this
-#
-#  m = size(a, 1)
-#
-#  a_parent = parent(a)
-#  b_parent = parent(b)
-#
-#  row_offset = first(a.indices[1]) - 1
-#  a_col_offset = first(a.indices[2]) - 1
-#  b_col_offset = first(b.indices[2]) - 1
-#
-#  nrows_a = size(a_parent, 1)
-#  nrows_b = size(b_parent, 1)
-#
-#  a_base_offset = a_col_offset * nrows_a + row_offset
-#  b_base_offset = b_col_offset * nrows_b + row_offset
-#
-#  threads = 256
-#  blocks  = cld(m, threads)
-#
-#  @cuda threads=threads blocks=blocks fast_copy_kernel!(
-#      a_parent, b_parent, m, a_base_offset, b_base_offset
-#  )
-#  return a
-#end
-#
-#function fast_copy_kernel!(a, b, len, a_offset, b_offset)
-#    i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-#    if i <= len
-#        @inbounds a[a_offset + i] = b[b_offset + i]
-#    end
-#    return nothing
-#end
-
 function LinearAlgebra.mul!(
     H::SubArray{T, 2, CuArray{T, 2, CUDA.DeviceMemory}, Tuple{UnitRange{Int64}, UnitRange{Int64}}, false},
     Hj::SubArray{T, 2, CuArray{T, 2, CUDA.DeviceMemory}, Tuple{UnitRange{Int64}, Base.Slice{Base.OneTo{Int64}}}, false},
     z::CuArray{T, 2, CUDA.DeviceMemory},
     α::Int, β::Bool) where T<:Union{Float32, Float64, ComplexF32, ComplexF64}
 
-  @assert α == -1 && β == 1 "This kernel is optimized for α=-1, β=1 only"
+  @assert size(H, 1) == size(Hj, 1)
+  @assert size(H, 2) == size(z, 2)
+  @assert size(Hj, 2) == size(z, 1)
 
-  m = size(Hj, 1)
-  k = size(Hj, 2)
-  n = size(z, 2)
-
-  @assert size(H, 1) == m
-  @assert size(H, 2) == n
-  @assert size(z, 1) == k
-
-  H_parent = parent(H)
-  Hj_parent = parent(Hj)
-
-  H_row_offset = first(H.indices[1]) - 1
-  H_col_offset = first(H.indices[2]) - 1
-  Hj_row_offset = first(Hj.indices[1]) - 1
-
-  threads = (16, 16)
-  blocks = (cld(m, 16), cld(n, 16))
-
-  @cuda threads=threads blocks=blocks _matmul_subtract_kernel!(
-    H_parent, Hj_parent, z, m, k, n, H_row_offset, H_col_offset, Hj_row_offset
-  )
+  # Use CUBLAS gemm! for: C = α * op(A) * op(B) + β * C
+  # We want: H = -1 * Hj * z + 1 * H (i.e., H -= Hj * z)
+  # gemm! signature: gemm!(tA, tB, alpha, A, B, beta, C)
+  # where tA, tB are 'N' (no transpose), 'T' (transpose), or 'C' (conjugate transpose)
+  CUDA.CUBLAS.gemm!('N', 'N', T(α), Hj, z, T(β), H)
 
   return H
-end
-
-function _matmul_subtract_kernel!(H_parent, Hj_parent, z, m, k, n,
-                                  H_row_off, H_col_off, Hj_row_off)
-  i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
-  j = (blockIdx().y - 1) * blockDim().y + threadIdx().y
-
-  if i <= m && j <= n
-    acc = zero(eltype(H_parent))
-
-    # 4x unrolled loop for better ILP
-    l = 1
-    @inbounds while l <= k - 7
-      acc += Hj_parent[Hj_row_off + i, l]     * z[l, j]
-      acc += Hj_parent[Hj_row_off + i, l + 1] * z[l + 1, j]
-      acc += Hj_parent[Hj_row_off + i, l + 2] * z[l + 2, j]
-      acc += Hj_parent[Hj_row_off + i, l + 3] * z[l + 3, j]
-      acc += Hj_parent[Hj_row_off + i, l + 4] * z[l + 4, j]
-      acc += Hj_parent[Hj_row_off + i, l + 5] * z[l + 5, j]
-      acc += Hj_parent[Hj_row_off + i, l + 6] * z[l + 6, j]
-      acc += Hj_parent[Hj_row_off + i, l + 7] * z[l + 7, j]
-      l += 8
-    end
-
-    @inbounds while l <= k
-      acc += Hj_parent[Hj_row_off + i, l] * z[l, j]
-      l += 1
-    end
-
-    # H[i,j] -= acc (single FMA operation)
-    @inbounds H_parent[H_row_off + i, H_col_off + j] -= acc
-  end
-
-  return nothing
 end
 
 function LinearAlgebra.mul!(
