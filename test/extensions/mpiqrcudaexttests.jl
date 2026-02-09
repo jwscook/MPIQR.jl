c6f8b1ee74986cdd8d444628358bae3d5b822bbd
James Cook <cookjws@gmail.com>
2026-02-09 17:43:43 +0000

    update test/ext... mpiqrcuda...

--- test/extensions/mpiqrcudaexttests.jl
+++ test/extensions/mpiqrcudaexttests.jl
@@ -13,8 +13,8 @@ const nts = Threads.nthreads()
 BLAS.set_num_threads(nts)
 
 using Random, ProgressMeter
-function run(blocksizes=(9,), npows=(10,11,12,13,14), Ts=(ComplexF64,); bestof=1)
-  for npow in npows, blocksize in blocksizes, T in Ts
+function run(blocksizes=(16,), npows=(10,11,12,13), Ts=(ComplexF64,); bestof=2)
+  for npow in npows, T in Ts
     Random.seed!(0)
     n = 2^npow
     m = n + 2^(npow-2)
@@ -33,42 +33,51 @@ function run(blocksizes=(9,), npows=(10,11,12,13,14), Ts=(ComplexF64,); bestof=1
       end
       t1 = minimum(t1s)
       BLAS.set_num_threads(nts)
+      Ac = CuArray(A0)
+      bc = CuArray(b0)
+      tcs = []
+      for _ in 1:bestof
+        push!(tcs, @elapsed qr!(Ac) \ bc)
+      end
+      tc = minimum(tcs)
     end
     x1 = MPI.bcast(x1, 0, cmm)
 
-    localcols = MPIQR.localcolumns(rnk, n, blocksize, sze)
-    b = deepcopy(b0)
+    for blocksize in blocksizes
+      localcols = MPIQR.localcolumns(rnk, n, blocksize, sze)
+      b = deepcopy(b0)
 
-    A = MPIQR.MPIQRMatrix(ArrayT(deepcopy(A0[:, localcols])), size(A0); blocksize=blocksize)
-    y2 = Array(A * ArrayT(x1))
-    if iszero(rnk)
-      @test y2 ≈ y1
-    end
-    MPI.Barrier(cmm)
-    dt = iszero(rnk) ? 1 : 2^31
-    x2 = ArrayT(similar(x1))
-    t2 = @elapsed begin
-      qrA = qr!(A, progress=Progress(A, dt=dt; showspeed=true), verbose=true)
-      ldiv!(x2, qrA, ArrayT(b); verbose=true, progress=Progress(A, dt=dt/10; showspeed=true))
-    end
-    x3 = qrA \ ArrayT(b)
-    localcols = MPIQR.localcolumns(qrA)
-    @test Array(x2) ≈ Array(x3)
+      A = MPIQR.MPIQRMatrix(ArrayT(deepcopy(A0[:, localcols])), size(A0); blocksize=blocksize)
+      y2 = Array(A * ArrayT(x1))
+      if iszero(rnk)
+        @test y2 ≈ y1
+      end
+      MPI.Barrier(cmm)
+      dt = iszero(rnk) ? 1 : 2^31
+      x2 = ArrayT(similar(x1))
+      t2 = @elapsed begin
+        qrA = qr!(A, progress=Progress(A, dt=dt; showspeed=true), verbose=true)
+        ldiv!(x2, qrA, ArrayT(b); verbose=true, progress=Progress(A, dt=dt/10; showspeed=true))
+      end
+      x3 = qrA \ ArrayT(b)
+      localcols = MPIQR.localcolumns(qrA)
+      @test Array(x2) ≈ Array(x3)
 
-    MPI.Barrier(cmm)
+      MPI.Barrier(cmm)
 
-    if iszero(rnk)
-        @test norm(A0' * A0 * Array(x1) .- A0' * b0) < 1e-8
-        res = norm(A0' * A0 * Array(x2) .- A0' * b0)
-        try
-          println("np=$sze, nt=$nts, T=$T, m=$m, n=$n, blocksize=$blocksize:")
-          println("tLAPACK = $t1, tMPIQR = $t2, ratio=$(t2/t1)x")
-          @test res < 1e-8
-          println("    PASSED: norm of residual = $res")
-        catch err
-            @warn err
-          println("    FAILED: norm of residual = $res")
-        end
+      if iszero(rnk)
+          @test norm(A0' * A0 * Array(x1) .- A0' * b0) < 1e-8
+          res = norm(A0' * A0 * Array(x2) .- A0' * b0)
+          try
+            println("np=$sze, nt=$nts, T=$T, m=$m, n=$n, blocksize=$blocksize:")
+            println("tLAPACK = $t1, tCUDA = $tc, tMPIQR = $t2, ratio_CUDA=$(t2/tc)x, ratio_LAPACK=$(t2/t1)x")
+            @test res < 1e-8
+            println("    PASSED: norm of residual = $res")
+          catch err
+              @warn err
+            println("    FAILED: norm of residual = $res")
+          end
+      end
     end
   end
 end
